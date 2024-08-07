1191a6c45c20d05b719fc53c77d231ba37a31884
Burcu Kamilcelebi <burcucelebi33@gmail.com>
2024-08-07 22:45:37 +0300

    added view&urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+
+def index(request): 
+    return HttpResponse("Home Page")
+
+def blogs(request): 
+    return HttpResponse("Blogs")
+
+def blog_details(request,id): 
+    return HttpResponse("Blog details" + id) 
