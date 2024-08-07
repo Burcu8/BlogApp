eb9364700833e43ba355ca69d1af8adc8dee9162
Burcu Kamilcelebi <burcucelebi33@gmail.com>
2024-08-07 23:21:53 +0300

    update index

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 def index(request): 
-    return HttpResponse("Home Page")
+    return render(request, "index.html")
 
 def blogs(request): 
     return HttpResponse("Blogs")
