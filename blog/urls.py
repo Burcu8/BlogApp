507e0c847638a6181c5a860f000663e01499cc83
Burcu Kamilcelebi <burcucelebi33@gmail.com>
2024-09-03 14:53:51 +0300

    page links have been created

--- blog/urls.py
+++ blog/urls.py
@@ -9,8 +9,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.index, name='home'),
     path("index", views.index, name='index'),
     path("blogs", views.blogs, name='blogs'),
-    path("blogs/<int:id>", views.blog_details, name='blog-details'),
+    path("blogs/<int:id>", views.blog_details, name='blog_details'),
 ]
